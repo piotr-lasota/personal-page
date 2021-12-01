cde63c9e2b0a7f57066934a67c3032149b4d1497
Piotr Lasota <lasota.piotr@gmail.com>
2021-12-01 21:52:48 +0100

    refactor: turn top-layout.js into top-layout.tsx

--- frontend/src/gatsby-theme-material-ui-top-layout/components/top-layout.tsx
+++ frontend/src/gatsby-theme-material-ui-top-layout/components/top-layout.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import ThemeTopLayout from 'gatsby-theme-material-ui-top-layout/src/components/top-layout';
-import { Box, Container } from '@mui/material';
+import { Box, Container, Theme } from '@mui/material';
+
 import NavigationBar from './NavigationBar';
 
+type TopLayoutProps = {
+  children: JSX.Element;
+  theme: Theme;
+};
+
 // eslint-disable-next-line
-const TopLayout = ({ children, theme }) => {
+const TopLayout = ({ children, theme }: TopLayoutProps) : JSX.Element => {
   return (
     <ThemeTopLayout theme={theme}>
       <Container maxWidth="md">

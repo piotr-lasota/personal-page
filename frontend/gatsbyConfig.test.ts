f2322591343bf4b8f34b27f8396084b7fed3e08b
Piotr Lasota <lasota.piotr@gmail.com>
2021-12-04 18:09:49 +0100

    test: refined to avoid collapsing an empty array to string by expect()

--- frontend/gatsbyConfig.test.ts
+++ frontend/gatsbyConfig.test.ts
@@ -4,27 +4,26 @@ import gatsbyConfig from './gatsby-config';
 
 const gatsbyPagesFolder = ['src', 'pages'];
 
+const pathToPage = (page: string): string =>
+  path.resolve(
+    __dirname,
+    ...gatsbyPagesFolder,
+    `${page === '' ? 'index' : page}.tsx`
+  );
+
 describe('Pages config', () => {
   it('contains a matching page source file for every page specified in siteMetadata', async () => {
     // Act
-    const missingPages = gatsbyConfig.siteMetadata.pages
-      .map((page) => {
-        const expectedPageComponentName =
-          page.link === '' ? 'index' : page.link;
-
-        const expectedPagePath = path.resolve(
-          __dirname,
-          ...gatsbyPagesFolder,
-          `${expectedPageComponentName}.tsx`
-        );
-
-        const fileExists = fs.existsSync(expectedPagePath);
-
-        return fileExists ? null : page.name;
-      })
-      .filter((missingPageName) => missingPageName !== null);
+    const missingPageNames = gatsbyConfig.siteMetadata.pages
+      .map((page) => ({
+        name: page.name,
+        exists: fs.existsSync(pathToPage(page.link))
+      }))
+      .filter((page) => !page.exists)
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map((missingPage) => missingPage.name);
 
     // Assert
-    expect(missingPages).toBe([]);
+    expect(missingPageNames).toEqual([]);
   });
 });

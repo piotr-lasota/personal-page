0d6c47941cf95922cdbab0e8d7b9278b2ff78060
Piotr Lasota <lasota.piotr@gmail.com>
2021-12-06 19:59:46 +0100

    feat: theming

--- frontend/src/gatsby-theme-material-ui-top-layout/theme.ts
+++ frontend/src/gatsby-theme-material-ui-top-layout/theme.ts
@@ -1,9 +1,21 @@
 import { createTheme, responsiveFontSizes } from '@mui/material';
+import { deepOrange, purple } from '@mui/material/colors';
 
 const theme = responsiveFontSizes(
   createTheme({
     palette: {
-      mode: 'light'
+      primary: {
+        light: '#ffb2ff',
+        main: '#ea80fc',
+        dark: '#b64fc8',
+        contrastText: '#000'
+      },
+      secondary: {
+        light: '#ffa06d',
+        main: '#ff6e40',
+        dark: '#c53d13',
+        contrastText: '#000'
+      }
     }
   })
 );

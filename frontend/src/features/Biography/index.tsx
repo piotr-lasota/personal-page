7cd9828579d484aac33e68d7ed837508a2923f68
Piotr Lasota <lasota.piotr@gmail.com>
2022-01-19 20:21:16 +0100

    feat: refined the biography

--- frontend/src/features/Biography/index.tsx
+++ frontend/src/features/Biography/index.tsx
@@ -4,6 +4,24 @@ import BiographyHeaderCell from './BiographyHeaderCell';
 import BiographyRow from './BiographyRow';
 import BiographyContentCell from './BiographyContentCell';
 
+type LifeStageHeaderProps = {
+  period: string;
+  label: string;
+};
+const LifeStageHeader = ({
+  period,
+  label
+}: LifeStageHeaderProps): JSX.Element => (
+  <>
+    <Typography variant="h4" align="center" fontWeight="light">
+      {period}
+    </Typography>
+    <Typography variant="h3" align="center" fontWeight="light">
+      {label}
+    </Typography>
+  </>
+);
+
 const Biography = (): JSX.Element => (
   <Grid component="section" container direction="column" spacing={10}>
     <Grid component="header" item xs={12}>
@@ -14,29 +32,26 @@ const Biography = (): JSX.Element => (
 
     <BiographyRow>
       <BiographyHeaderCell>
-        <Typography variant="h2" align="center">
-          First Left
-        </Typography>
+        <LifeStageHeader period="'18 and beyond" label="Software Developer" />
       </BiographyHeaderCell>
 
       <BiographyContentCell>
         <Typography align="justify">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec
-          lobortis purus, sed blandit neque. Nulla iaculis velit eu risus
-          efficitur sodales. Cras tempus sed urna ac sollicitudin. Sed porta
-          purus ac dictum vehicula. Nam sagittis lobortis accumsan. In in
-          sollicitudin tortor, mattis vestibulum libero. Praesent felis dolor,
-          interdum vel metus eu, dignissim rhoncus dolor. Etiam consequat metus
-          tellus, sit amet viverra mauris tempus posuere.
+          After realizing that mechanical engineering is not my thing I decided
+          to deep dive into professional software development. And it was a head
+          first dive. I quit my well-paid job, placed all my eggs in one basked
+          and decided that if I&apos;m to fail miserably then at least it will
+          be trying to do something that I wanted to try for a long time. I fell
+          in love with everything code (although it being unrequited love at
+          first) and gradually gained skills and knowledge to become an
+          autonomous specialist able to join any team on any product.
         </Typography>
       </BiographyContentCell>
     </BiographyRow>
 
     <BiographyRow reverseRowAboveMedium>
       <BiographyHeaderCell>
-        <Typography variant="h2" align="center">
-          Second Right
-        </Typography>
+        <LifeStageHeader period="'14 - '18" label="Mechanical Engineer" />
       </BiographyHeaderCell>
 
       <BiographyContentCell>
@@ -61,9 +76,7 @@ const Biography = (): JSX.Element => (
 
     <BiographyRow>
       <BiographyHeaderCell>
-        <Typography variant="h2" align="center">
-          Third Left
-        </Typography>
+        <LifeStageHeader period="'10 - '15" label="Student" />
       </BiographyHeaderCell>
 
       <BiographyContentCell>
@@ -75,30 +88,6 @@ const Biography = (): JSX.Element => (
           sollicitudin tortor, mattis vestibulum libero. Praesent felis dolor,
           interdum vel metus eu, dignissim rhoncus dolor. Etiam consequat metus
           tellus, sit amet viverra mauris tempus posuere.
-        </Typography>
-      </BiographyContentCell>
-    </BiographyRow>
-
-    <BiographyRow reverseRowAboveMedium>
-      <BiographyHeaderCell>
-        <Typography variant="h2" align="center">
-          Fourth Left
-        </Typography>
-      </BiographyHeaderCell>
-
-      <BiographyContentCell>
-        <Typography align="justify">
-          Nulla iaculis velit eu risus efficitur sodales. Cras tempus sed urna
-          ac sollicitudin. Sed porta purus ac dictum vehicula. Nam sagittis
-          lobortis accumsan. In in sollicitudin tortor, mattis vestibulum
-          libero. Praesent felis dolor, interdum vel metus eu, dignissim rhoncus
-          dolor. Etiam consequat metus tellus, sit amet viverra mauris tempus
-          posuere. Nulla iaculis velit eu risus efficitur sodales. Cras tempus
-          sed urna ac sollicitudin. Sed porta purus ac dictum vehicula. Nam
-          sagittis lobortis accumsan. In in sollicitudin tortor, mattis
-          vestibulum libero. Praesent felis dolor, interdum vel metus eu,
-          dignissim rhoncus dolor. Etiam consequat metus tellus, sit amet
-          viverra mauris tempus posuere.
         </Typography>
       </BiographyContentCell>
     </BiographyRow>

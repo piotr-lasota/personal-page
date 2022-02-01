ca714fd8862c883ca9f0021db92f94c3206512be
Piotr Lasota <lasota.piotr@gmail.com>
2022-02-01 21:47:24 +0100

    chore: fix faulty workaround for https://github.com/hashicorp/terraform-provider-azurerm/issues/14750

--- infra/website/custom_name/main.tf
+++ infra/website/custom_name/main.tf
@@ -10,6 +10,6 @@ resource "azurerm_dns_txt_record" "txt-root" {
   resource_group_name = var.resource_group_name
   ttl                 = var.ttl
   record {
-    value = try(var.txt_validation_token, azurerm_static_site_custom_domain.txt-root.validation_token)
+    value = var.txt_validation_token != null ? var.txt_validation_token : azurerm_static_site_custom_domain.txt-root.validation_token
   }
 }

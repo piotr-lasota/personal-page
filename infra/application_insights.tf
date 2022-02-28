0908ab05ec4a6d818b0cfab0bf72b460826e3f18
Piotr Lasota <lasota.piotr@gmail.com>
2022-02-28 19:33:26 +0100

    chore: terraform azurerm provider upgraded

--- infra/application_insights.tf
+++ infra/application_insights.tf
@@ -8,10 +8,11 @@ resource "azurerm_log_analytics_workspace" "homepage" {
 }
 
 resource "azurerm_application_insights" "website" {
-  name                = "ai-${local.resources_core_name}"
-  application_type    = "web"
-  location            = azurerm_resource_group.homepage.location
-  resource_group_name = azurerm_resource_group.homepage.name
-  workspace_id        = azurerm_log_analytics_workspace.homepage.id
-  retention_in_days   = azurerm_log_analytics_workspace.homepage.retention_in_days
+  name                                = "ai-${local.resources_core_name}"
+  force_customer_storage_for_profiler = false
+  application_type                    = "web"
+  location                            = azurerm_resource_group.homepage.location
+  resource_group_name                 = azurerm_resource_group.homepage.name
+  workspace_id                        = azurerm_log_analytics_workspace.homepage.id
+  retention_in_days                   = azurerm_log_analytics_workspace.homepage.retention_in_days
 }
